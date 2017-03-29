05885aefc0502d002c7361519ac1213f2b588859
longgeek <longgeek@fuvism.com>
2017-03-29 10:28:07 +0800

    make the python setup.py
    Change-Id: I268e50db59a5fc7d663f7e8d1c3c68c9231dc340

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name='fuerteclient',
+    version='1.0.0',
+    author='Longgeek',
+    author_email='longgeek@fuvism.com',
+    url='http://www.longgeek.com',
+    description='Fuerte Client'
+)
